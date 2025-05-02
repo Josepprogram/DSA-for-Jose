6777a200793fac3bd1815026349e8b8f0cff87a6
juanmoreno <juanmandresv@gmail.com>
2025-05-02 12:44:04 -0400

    Solucion Rectangulo

---
+++ functions_problems/functs_solutions/Rectangle.py
@@ -0,0 +1,12 @@
+#Se te proporciona el largo y el ancho de un rectángulo. 
+#Tu tarea es crear una funcion que calcule y muestre el área del rectángulo.
+
+print("Coloque el largo del rectangulo")
+largo=float(input())
+print("Coloque el ancho del rectangulo")
+ancho=float(input())
+
+area_rec=largo*ancho
+
+print(f"Este es el area del rectangulo {area_rec}")
+
